bdb8403671bd1bd4200d09f601d6c17d713515b1
MrEveKS <45174718+MrEveKS@users.noreply.github.com>
2018-11-30 20:30:38 +0700

    update

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ module.exports = "/* test */\r\n.map-container {\r\n  width: 100%;\r\n  height: 
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!--The content below is only a placeholder and can be replaced.-->\r\n<div style=\"text-align:center\">\r\n\r\n  <h1>\r\n    Welcome to {{ title }}!\r\n  </h1>\r\n\r\n  <div class=\"map-container\" appGoogleMap></div>\r\n</div>\r\n\r\n"
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\r\n<div style=\"text-align:center\">\r\n  <h1>\r\n    Welcome to {{ title }}!\r\n  </h1>\r\n\r\n  <div class=\"map-container\" appGoogleMap></div>\r\n</div>\r\n\r\n"
 
 /***/ }),
 
@@ -352,10 +352,18 @@ var GoogleMapDirective = /** @class */ (function () {
     };
     GoogleMapDirective.prototype.placeMarkerAndPanTo = function (latLng) {
         var marker = new google.maps.Marker({
+            icon: {
+                // github icon >_<
+                path: "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z",
+                fillColor: '#999',
+                fillOpacity: 1,
+                strokeColor: '#000',
+                strokeWeight: 2,
+                scale: 1,
+            },
             position: latLng,
             map: this._map
         });
-        this._map.panTo(latLng);
     };
     GoogleMapDirective = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Directive"])({
